d41fa5af4aff5cde90e2f8d00fa9dfc3f9cba532
japespszx <japespszx@live.com.ph>
2017-11-06 01:29:08 +0800

    - Added layer attribute - Fixed some export problems

--- Display.java
+++ Display.java
@@ -70,7 +70,7 @@ public class Display extends JFrame {
         button.addMouseMotionListener(draggerForButton);
 
         compile = new JButton("Compile");
-        compile.setBounds(0, 540, 1160, 30);
+        compile.setBounds(0, 541, 1160, 30);
         compile.setFont(font);
         compile.addActionListener(e -> {
             export();
@@ -80,7 +80,7 @@ public class Display extends JFrame {
         panel.add(label);
         panel.add(tf);
         panel.add(button);
-       
+
 
         dragger = new Dragger(label, draggerForTextField, draggerForButton, objects);
 
@@ -95,7 +95,7 @@ public class Display extends JFrame {
         changeHeightLabel = new JLabel("Change Height:");
         changeHeightLabel.setBounds(0, 100, 200, 30);
         changeHeightLabel.setFont(font);
-        
+
         changeTextLabel = new JLabel("Change Text:");
         changeTextLabel.setBounds(0, 180, 200, 30);
         changeTextLabel.setFont(font);
@@ -107,7 +107,7 @@ public class Display extends JFrame {
         tfheight = new JTextField();
         tfheight.setBounds(0, 130, 100, 30);
         tfheight.setFont(font);
-        
+
         tfText = new JTextField();
         tfText.setBounds(0, 210, 100, 30);
         tfText.setFont(font);
@@ -115,21 +115,21 @@ public class Display extends JFrame {
         changeButton = new JButton("Change");
         changeButton.setBounds(0, 300, 150, 30);
         changeButton.setFont(font);
-        
+
         load = new JButton("load Json file");
         load.setBounds(0, 400, 150, 30);
         load.setFont(font);
         load.addActionListener(e -> {
-        	loadJson();
+            loadJson();
         });
-        
+
 
         MyActionListener listener = new MyActionListener(dragger, draggerForTextField, draggerForButton, tfwidth, tfheight, tfText);
 
         changeButton.addActionListener(listener);
 
         changePanel = new JPanel();
-        changePanel.setBounds(960, 0, 200, 540);
+        changePanel.setBounds(961, 0, 200, 540);
         changePanel.add(changeWidthLabel);
         changePanel.add(tfwidth);
         changePanel.add(changeHeightLabel);
@@ -173,32 +173,36 @@ public class Display extends JFrame {
         labels.add("\t\t\"labels\": [");
         buttons.add("\t\t\"buttons\": [");
         textFields.add("\t\t\"textfields\": [");
+        int layer = 1;
         for (Object o :
                 objects) {
             if (o instanceof JLabel) {
                 JLabel jl = (JLabel) o;
                 labels.add("\t\t\t{\"text\": \"" + jl.getText() + "\", " +
-                        "\"font\": " + jl.getFont().getFamily() + ", " +
+                        "\"font\": \"" + jl.getFont().getFamily() + "\", " +
                         "\"xposition\": " + jl.getX() + ", " +
                         "\"yposition\": " + jl.getY() + ", " +
                         "\"width\": " + jl.getWidth() + ", " +
-                        "\"height\": " + jl.getHeight() + "},");
+                        "\"height\": " + jl.getHeight() + ", " +
+                        "\"layer\": " + layer++ + "},");
             } else if (o instanceof JButton) {
                 JButton jb = (JButton) o;
                 buttons.add("\t\t\t{\"text\": \"" + jb.getText() + "\", " +
-                        "\"font\": " + jb.getFont().getFamily() + ", " +
+                        "\"font\": \"" + jb.getFont().getFamily() + "\", " +
                         "\"xposition\": " + jb.getX() + ", " +
                         "\"yposition\": " + jb.getY() + ", " +
                         "\"width\": " + jb.getWidth() + ", " +
-                        "\"height\": " + jb.getHeight() + "},");
+                        "\"height\": " + jb.getHeight() + ", " +
+                        "\"layer\": " + layer++ + "},");
             } else if (o instanceof JTextField) {
                 JTextField jt = (JTextField) o;
                 textFields.add("\t\t\t{\"text\": \"" + jt.getText() + "\", " +
-                        "\"font\": " + jt.getFont().getFamily() + ", " +
+                        "\"font\": \"" + jt.getFont().getFamily() + "\", " +
                         "\"xposition\": " + jt.getX() + ", " +
                         "\"yposition\": " + jt.getY() + ", " +
                         "\"width\": " + jt.getWidth() + ", " +
-                        "\"height\": " + jt.getHeight() + "},");
+                        "\"height\": " + jt.getHeight() + ", " +
+                        "\"layer\": " + layer++ + "},");
             }
         }
         String s;
@@ -232,102 +236,98 @@ public class Display extends JFrame {
             e.printStackTrace();
         }
     }
-    
-    public void loadJson(){
-    	JFileChooser fileChooser = new JFileChooser();
-    	fileChooser.setDialogTitle("Load Json file");
-    	File workingDirectory = new File(System.getProperty("user.dir"));
-    	fileChooser.setCurrentDirectory(workingDirectory);
-    	
-    	 int returnValue = fileChooser.showOpenDialog(null);
-         if (returnValue == JFileChooser.APPROVE_OPTION) {
-           File selectedFile = fileChooser.getSelectedFile();
-           String jsonString = readAllBytes(selectedFile.getPath());
-           
-           JsonToUI(jsonString);
-         }
+
+    public void loadJson() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Load Json file");
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        fileChooser.setCurrentDirectory(workingDirectory);
+
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            String jsonString = readAllBytes(selectedFile.getPath());
+
+            JsonToUI(jsonString);
+        }
     }
-    
-    private void JsonToUI(String jsonString){
-    	JsonElement jelement = new JsonParser().parse(jsonString);
-        JsonObject  jobject = jelement.getAsJsonObject();
+
+    private void JsonToUI(String jsonString) {
+        JsonElement jelement = new JsonParser().parse(jsonString);
+        JsonObject jobject = jelement.getAsJsonObject();
         jobject = jobject.getAsJsonObject("webpage");
-        
+
         JsonArray jarray = jobject.getAsJsonArray("labels");
-        if(jarray != null){
-	        for(int i = 0; i < jarray.size(); i++){
-	        	jobject = jarray.get(i).getAsJsonObject();
-	            String text = jobject.get("text").toString();
-	            text = text.substring(1,text.length()-1);
-	            String x = jobject.get("xposition").toString();
-	            String y = jobject.get("yposition").toString();
-	            String width = jobject.get("width").toString();
-	            String height = jobject.get("height").toString();
-	            
-	            JLabel label = new JLabel(text); 
-	            label.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
-	            panel.add(label);
-	            label.setFont(font);
-	            objects.add(label);
-	        }
+        if (jarray != null) {
+            for (int i = 0; i < jarray.size(); i++) {
+                jobject = jarray.get(i).getAsJsonObject();
+                String text = jobject.get("text").toString();
+                text = text.substring(1, text.length() - 1);
+                String x = jobject.get("xposition").toString();
+                String y = jobject.get("yposition").toString();
+                String width = jobject.get("width").toString();
+                String height = jobject.get("height").toString();
+
+                JLabel label = new JLabel(text);
+                label.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
+                panel.add(label);
+                label.setFont(font);
+                objects.add(label);
+            }
         }
-        
+
         jobject = jelement.getAsJsonObject();
         jobject = jobject.getAsJsonObject("webpage");
-        
+
         jarray = jobject.getAsJsonArray("buttons");
-        
-        if(jarray != null){
-	        for(int i = 0; i < jarray.size(); i++){
-	        	jobject = jarray.get(i).getAsJsonObject();
-	            String text = jobject.get("text").toString();
-	            text = text.substring(1,text.length()-1);
-	            String x = jobject.get("xposition").toString();
-	            String y = jobject.get("yposition").toString();
-	            String width = jobject.get("width").toString();
-	            String height = jobject.get("height").toString();
-	            
-	            JButton button = new JButton(text); 
-	            button.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
-	            panel.add(button);
-	            button.setFont(font);
-	            objects.add(button);
-	        }
+
+        if (jarray != null) {
+            for (int i = 0; i < jarray.size(); i++) {
+                jobject = jarray.get(i).getAsJsonObject();
+                String text = jobject.get("text").toString();
+                text = text.substring(1, text.length() - 1);
+                String x = jobject.get("xposition").toString();
+                String y = jobject.get("yposition").toString();
+                String width = jobject.get("width").toString();
+                String height = jobject.get("height").toString();
+
+                JButton button = new JButton(text);
+                button.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
+                panel.add(button);
+                button.setFont(font);
+                objects.add(button);
+            }
         }
-        
+
         jobject = jelement.getAsJsonObject();
         jobject = jobject.getAsJsonObject("webpage");
-        
+
         jarray = jobject.getAsJsonArray("textfields");
-        
-        if(jarray != null){
-	        for(int i = 0; i < jarray.size(); i++){
-	        	jobject = jarray.get(i).getAsJsonObject();
-	            String text = jobject.get("text").toString();
-	            text = text.substring(1,text.length()-1);
-	            String x = jobject.get("xposition").toString();
-	            String y = jobject.get("yposition").toString();
-	            String width = jobject.get("width").toString();
-	            String height = jobject.get("height").toString();
-	            
-	            JTextField tf = new JTextField(text); 
-	            tf.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
-	            panel.add(tf);
-	            tf.setFont(font);
-	            objects.add(tf);
-	        }
+
+        if (jarray != null) {
+            for (int i = 0; i < jarray.size(); i++) {
+                jobject = jarray.get(i).getAsJsonObject();
+                String text = jobject.get("text").toString();
+                text = text.substring(1, text.length() - 1);
+                String x = jobject.get("xposition").toString();
+                String y = jobject.get("yposition").toString();
+                String width = jobject.get("width").toString();
+                String height = jobject.get("height").toString();
+
+                JTextField tf = new JTextField(text);
+                tf.setBounds(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(width), Integer.parseInt(height));
+                panel.add(tf);
+                tf.setFont(font);
+                objects.add(tf);
+            }
         }
     }
-    
-    private static String readAllBytes(String filePath)
-    {
+
+    private static String readAllBytes(String filePath) {
         String content = "";
-        try
-        {
-            content = new String ( Files.readAllBytes( Paths.get(filePath) ) );
-        }
-        catch (IOException e)
-        {
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return content;
